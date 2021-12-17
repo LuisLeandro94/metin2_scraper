eebec1d58d29ebde33cc96260ebdfd7a9d479a36
Luis Leandro <elio@codepoint.pt>
2021-12-17 21:50:52 +0000

    chore: bot running the full pagination now

--- index.js
+++ index.js
@@ -1,26 +1,80 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
+import chalk from 'chalk'
+import axios from 'axios'
+import cheerio  from 'cheerio'
+import fs from 'fs'
+import dns from 'dns'
+import retry from 'retry'
 
-async function main() {
-  const result = await request.get("https://m2.ongame.net/ranking/infernal/");
-  const $ = cheerio.load(result);
-  const scrapedData = [];
-  $("body > div > div > main > section > div > table > tbody > tr ").each((index, element) => {
-    const tds = $(element).find("td");
-    const rank = $(tds[0]).text();
-    const floor = $(tds[1]).text();
-    const nickname = $(tds[2]).text();
-    const time = $(tds[3]).text();
-    const date = $(tds[5]).text();
+const baseUrl = 'https://m2.ongame.net/ranking/infernal/'
+const outputFile = 'data.json'
+const parsedResults = []
+const pageLimit = 13
+let pageCounter = 1
+let resultCount = 0
+var paginatedLink = ''
 
-    const tableRow = { rank, floor, nickname, time, date }
+console.log(chalk.yellow.bgWhite(`\n  Scraping of ${chalk.underline.bold(baseUrl)} initiated...\n`))
 
-    scrapedData.push(tableRow)
-  });
-  var parsedHTML = $("body > div > div > main > section > nav > ul > li > a:last-child");
-  var listLength = parsedHTML.length - 1;
-  var nextButton = parsedHTML[listLength]
-  console.log(nextButton);
+const getWebsiteContent = async (url) => {
+  try {
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+
+    // New Lists
+    $('body > div > div > main > section > div > table > tbody > tr').each((i, el) => {
+      const count = resultCount++
+      const tds = $(el).find("td");
+      const rank = $(tds[0]).text();
+      const floor = $(tds[1]).text();
+      const nickname = $(tds[2]).text();
+      const time = $(tds[3]).text();
+      const date = $(tds[5]).text();
+
+      const tableRow = { rank, floor, nickname, time, date }
+      parsedResults.push(tableRow)
+    })
+
+    // Pagination Elements Link
+    var nextPageLink = '';
+    nextPageLink = baseUrl + '?page=' + (pageCounter + 1) + '#search';
+    console.log(chalk.cyan(`  Scraping: ${nextPageLink}`))
+    pageCounter++
+
+    if (pageCounter === pageLimit) {
+      exportResults(parsedResults)
+      return false
+    }
+
+    setTimeout(() => {
+      getWebsiteContent(nextPageLink)
+    }, 10000)
+  } catch (error) {
+    exportResults(parsedResults)
+    console.log('Uncaught')
+  }
 }
 
-main();
+const exportResults = (parsedResults) => {
+  fs.writeFile(outputFile, JSON.stringify(parsedResults, null, 4), (err) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log(chalk.yellow.bgWhite(`\n ${chalk.underline.bold(parsedResults.length)} Results exported successfully to ${chalk.underline.bold(outputFile)}\n`))
+  })
+}
+
+function faultTolerantResolve(address, cb) {
+  var operation = retry.operation();
+
+  operation.attempt(function(currentAttempt) {
+    dns.resolve(address, function(err, addresses) {
+      if (operation.retry(err)) {
+        return;
+      }
+
+      cb(err ? operation.mainError() : null, addresses);
+    });
+  });
+}
+
+getWebsiteContent(baseUrl)
